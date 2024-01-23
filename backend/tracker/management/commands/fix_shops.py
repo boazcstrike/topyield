cea5d6a00548a09e854c0a58d667110f64d75bc0
Boaz Sze <boaz.sze@gmail.com>
2024-01-24 01:29:29 +0800

    added extra

--- backend/tracker/management/commands/fix_shops.py
+++ backend/tracker/management/commands/fix_shops.py
@@ -1,3 +1,4 @@
+import json
 import pprint
 from django.core.management.base import BaseCommand
 from django.db.models import Count
@@ -9,6 +10,43 @@ from tracker.models import Shop
 class Command(BaseCommand):
   help = 'process the tracker app'
 
+  def get_unique_values(self, arr: list) -> list:
+    unique_values = set()
+    for sublist in arr:
+        for dictionary in sublist:
+            for key, value_dict in dictionary.items():
+                unique_values.add(key)
+                for value in value_dict.keys():
+                    unique_values.add(value)
+    print(list(unique_values))
+    return list(unique_values)
+
+  def map_decision(self, shop_similarities: list) -> list:
+    decision_map = []
+    for shop in shop_similarities:
+      decision = input(f'keep {shop}? (y/n)')
+      while decision != 'y' or decision != 'n':
+        if decision == 'y':
+          decision_map.append({shop: True})
+        elif decision == 'n':
+          decision_map.append({shop: False})
+        else:
+          decision_map.append({shop: True})
+    print(decision_map)
+    return decision_map
+
+  def execute_decision(self, decision_map: list):
+    for decision in decision_map:
+      for shop, keep in decision.items():
+        if keep:
+          print(f'keeping {shop}')
+        else:
+          print(f'deleting {shop}')
+
+  def process_cleaning(self, shop_similarities):
+    shop_similarities = self.get_unique_values(shop_similarities)
+    decision_map = self.map_decision(shop_similarities)
+    self.execute_decision(decision_map)
 
   def handle(self, *args, **options):
     print('fixing shops...')
@@ -27,15 +65,17 @@ class Command(BaseCommand):
     print('done cleaning dupes')
 
     shop_list = Shop.objects.values_list('name', flat=True)
-    print('counting the occurrences of each shop')
-    shop_counts = {}
-    for shop in shop_list:
-      shop_counts[shop] = shop_counts.get(shop, 0) + 1
 
-    print('finding the main shops with more repeated values')
-    main_shops = []
+    print('starting cleaning process...')
+
     for shop in shop_list:
-      pprint.pprint(is_name_similar(shop, shop_list), indent=2)
+      shop_similarities = is_name_similar(shop, shop_list)
+      if shop_similarities:
+        pprint.pprint(json.dumps(shop_similarities, indent=2))
+        self.process_cleaning(shop_similarities)
+        break
+
+
 
 
 
